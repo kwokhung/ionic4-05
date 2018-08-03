43ae5fb62991cce4928b94909b6b127c7887aaa4
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-08-03 15:17:39 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
 
-  public appPages = [
+  public appMenuItems = [
     {
       title: 'Home',
       url: '/home',
