f3f66615d3d9a310d497bbae1b1f2cc6fb2447dd
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-08-03 15:58:29 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,21 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+export interface MenuItem {
+  title: string;
+  url: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
 
-  public appMenuItems = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'Scan',
-      url: '/scan',
-      icon: 'qr-scanner'
-    }
+  appMenuItems: Array<MenuItem> = [
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Scan', url: '/scan', icon: 'qr-scanner' }
   ];
 
   constructor(
