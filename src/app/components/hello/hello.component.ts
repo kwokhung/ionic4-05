1450bb523bf2ba6cb781b011e245cfbe7788e272
kwokhung <chu_kwokhung@yahoo.com.hk>
2018-08-07 14:13:43 +0800

    init

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Renderer, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer) {    
+    let nativeElement = this.element.nativeElement;
+    renderer.setElementStyle(nativeElement, "color", "blue");
+  }
 
   ngOnInit() {
   }
